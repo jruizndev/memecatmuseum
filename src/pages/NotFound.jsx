56f9da7ad2d33d3075c796c0501c93b9c793e7f4
juamrm <ajulianaamorim@gmail.com>
2024-09-10 18:32:49 +0200

    video updated

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,24 +1,23 @@
-import { useRouteError, Link } from "react-router-dom";
 import React from "react";
 
-function MainContent() {
+function NotFound() {
   return (
     <main className="self-end mt-14 w-full max-w-[1318px] max-md:mt-10 max-md:max-w-full">
       <div className="flex gap-5 max-md:flex-col items-center">
-    
+        {/* Image Section */}
         <section className="flex flex-col w-[60%] max-md:w-full items-center">
           <img
             loading="lazy"
-            src="src/assets/animaciones/404.svg
-            className="object-contain w-full rounded-lg aspect-[16/9] max-md:aspect-auto max-md:w-full max-md:h-auto"
-            alt="Mecat Museum exhibit"
+            src="..src/assets/video cat404.mp4"
+            className="object-contain w-full rounded-lg max-md:aspect-auto max-md:w-full max-md:h-auto"
+            alt="Mecat Museum 404"
           />
         </section>
 
         {/* Text Section */}
         <section className="flex flex-col w-[40%] text-center max-md:w-full">
           <h1 className="text-7xl font-bold text-blue-500 uppercase leading-tight tracking-wider max-md:text-4xl max-md:leading-snug">
-            Mecat museum
+            Mecat Museum
           </h1>
         </section>
       </div>
@@ -26,4 +25,4 @@ function MainContent() {
   );
 }
 
-export default MainContent;
+export default NotFound;
