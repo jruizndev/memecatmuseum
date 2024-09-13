d3ffc5c099aa2acfef54110e35f283d61c74aed6
merkandez <131488754+merkandez@users.noreply.github.com>
2024-09-13 16:25:17 +0200

    cambio Footer en Layout

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,14 +1,14 @@
-import { Outlet } from 'react-router-dom'
-import Nav from '../components/Nav/Nav' // Importa la barra de navegación
+import { Outlet } from 'react-router-dom';
+import Nav from '../components/Nav/Nav'; // Importa la barra de navegación
 
 const Layout = () => {
-    return (
-        <>
-            <Nav /> {/* Aquí se incluye la barra de navegación que creamos */}
-            <Outlet />
-            <footer>Mi footer</footer>
-        </>
-    )
-}
+  return (
+    <>
+      <Nav /> {/* Aquí se incluye la barra de navegación que creamos */}
+      <Outlet />
+      <footer />
+    </>
+  );
+};
 
 export default Layout;
