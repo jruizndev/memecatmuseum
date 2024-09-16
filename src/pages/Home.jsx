fd994ceb23b3c89ede1fea7e78eef01e5ae96010
dario3111 <dario3pacheco@gmail.com>
2024-09-16 14:50:05 +0200

    merge rama dev

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,16 @@
-import React, { useState, useEffect, useContext } from 'react'
-import { getMemes, deleteMeme, getMemeByCategory } from '../services/services'
-import MemeGrid from '../components/MemeGrid'
-import Modal from '../components/Modal'
-import CreateMeme from '../pages/CreateMeme'
-import FilterContext from '../layout/FilterContext'
+import React, { useState, useEffect, useContext } from "react";
+import { getMemes, deleteMeme, getMemeByCategory } from "../services/services";
+import MemeGrid from "../components/MemeGrid";
+import Modal from "../components/Modal";
+import CreateMeme from "../pages/CreateMeme";
+import FilterContext from "../layout/FilterContext";
 
 const categories = [
-    'gatos_siendo_gatos1',
-    'gatos_siendo_humanos2',
-    'gatos_enfadados3',
-    'me_dijiste4',
-]
+  "gatos_siendo_gatos1",
+  "gatos_siendo_humanos2",
+  "gatos_enfadados3",
+  "me_dijiste4",
+];
 
 const categoryClasses = {
   gatos_siendo_gatos1: "bg-gatos-siendo-gatos1",
@@ -27,109 +27,109 @@ const categoryTitles = {
 };
 
 const Home = () => {
-    const {
-        selectedCategory,
-        selectedPopularity,
-        selectedDate,
-        handleSelectChange,
-    } = useContext(FilterContext)
+  const {
+    selectedCategory,
+    selectedPopularity,
+    selectedDate,
+    handleSelectChange,
+  } = useContext(FilterContext);
 
-    const [memesByCategory, setMemesByCategory] = useState({})
-    const [isModalOpen, setIsModalOpen] = useState(false)
-    const [filteredMemes, setFilteredMemes] = useState([])
+  const [memesByCategory, setMemesByCategory] = useState({});
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [filteredMemes, setFilteredMemes] = useState([]);
 
-    useEffect(() => {
-        const fetchMemes = async () => {
-            try {
-                const allMemes = await getMemes()
+  useEffect(() => {
+    const fetchMemes = async () => {
+      try {
+        const allMemes = await getMemes();
 
-                // Agrupamos y ordenamos los memes por categoría y popularidad
-                const memesByCategory = allMemes.reduce((acc, meme) => {
-                    if (!acc[meme.category]) {
-                        acc[meme.category] = []
-                    }
-                    acc[meme.category].push(meme)
-                    return acc
-                }, {})
+        // Agrupamos y ordenamos los memes por categoría y popularidad
+        const memesByCategory = allMemes.reduce((acc, meme) => {
+          if (!acc[meme.category]) {
+            acc[meme.category] = [];
+          }
+          acc[meme.category].push(meme);
+          return acc;
+        }, {});
 
-                // Ordenamos los memes dentro de cada categoría por popularidad (de mayor a menor)
-                for (const category in memesByCategory) {
-                    memesByCategory[category].sort((a, b) => b.likes - a.likes)
-                }
-
-                setMemesByCategory(memesByCategory)
-                setFilteredMemes(allMemes) // Inicialmente mostramos todos los memes
-            } catch (error) {
-                console.error('Error fetching memes:', error)
-            }
-        }
-
-        fetchMemes()
-    }, [])
-
-    // Función para filtrar y ordenar los memes
-    const filterAndSortMemes = (
-        allMemes,
-        selectedCategory,
-        selectedPopularity,
-        selectedDate
-    ) => {
-        let filteredMemes = [...allMemes] // Copia de todos los memes
-
-        // Filtramos por categoría si es diferente de "Todas"
-        if (selectedCategory !== 'Todas') {
-            filteredMemes = filteredMemes.filter(
-                (meme) => meme.category === selectedCategory
-            )
-        }
-
-        // Agrupamos los memes por categoría
-        const memesByCategory = filteredMemes.reduce((acc, meme) => {
-            if (!acc[meme.category]) {
-                acc[meme.category] = []
-            }
-            acc[meme.category].push(meme)
-            return acc
-        }, {})
-
-        // Ordenamos los memes dentro de cada categoría según los filtros
+        // Ordenamos los memes dentro de cada categoría por popularidad (de mayor a menor)
         for (const category in memesByCategory) {
-            if (selectedPopularity === 'Más populares') {
-                memesByCategory[category].sort((a, b) => b.likes - a.likes)
-            } else if (selectedPopularity === 'Menos populares') {
-                memesByCategory[category].sort((a, b) => a.likes - b.likes)
-            }
-
-            if (selectedDate === 'Más recientes') {
-                memesByCategory[category].sort(
-                    (a, b) => new Date(b.date) - new Date(a.date)
-                )
-            } else if (selectedDate === 'Más viejunos') {
-                memesByCategory[category].sort(
-                    (a, b) => new Date(a.date) - new Date(b.date)
-                )
-            }
+          memesByCategory[category].sort((a, b) => b.likes - a.likes);
         }
 
-        // Volvemos a aplanar el objeto para obtener un array de memes filtrados y ordenados
-        return Object.values(memesByCategory).flat()
+        setMemesByCategory(memesByCategory);
+        setFilteredMemes(allMemes); // Inicialmente mostramos todos los memes
+      } catch (error) {
+        console.error("Error fetching memes:", error);
+      }
+    };
+
+    fetchMemes();
+  }, []);
+
+  // Función para filtrar y ordenar los memes
+  const filterAndSortMemes = (
+    allMemes,
+    selectedCategory,
+    selectedPopularity,
+    selectedDate
+  ) => {
+    let filteredMemes = [...allMemes]; // Copia de todos los memes
+
+    // Filtramos por categoría si es diferente de "Todas"
+    if (selectedCategory !== "Todas") {
+      filteredMemes = filteredMemes.filter(
+        (meme) => meme.category === selectedCategory
+      );
     }
 
-    // Aplicamos filtros cada vez que cambiamos
-    useEffect(() => {
-        // Otenem memes combinados
-        const allMemes = Object.values(memesByCategory).flat()
+    // Agrupamos los memes por categoría
+    const memesByCategory = filteredMemes.reduce((acc, meme) => {
+      if (!acc[meme.category]) {
+        acc[meme.category] = [];
+      }
+      acc[meme.category].push(meme);
+      return acc;
+    }, {});
 
-        // Llamamos a la función de filtrado y ordenamiento
-        const filtered = filterAndSortMemes(
-            allMemes,
-            selectedCategory,
-            selectedPopularity,
-            selectedDate
-        )
+    // Ordenamos los memes dentro de cada categoría según los filtros
+    for (const category in memesByCategory) {
+      if (selectedPopularity === "Más populares") {
+        memesByCategory[category].sort((a, b) => b.likes - a.likes);
+      } else if (selectedPopularity === "Menos populares") {
+        memesByCategory[category].sort((a, b) => a.likes - b.likes);
+      }
 
-        setFilteredMemes(filtered)
-    }, [selectedCategory, selectedPopularity, selectedDate, memesByCategory])
+      if (selectedDate === "Más recientes") {
+        memesByCategory[category].sort(
+          (a, b) => new Date(b.date) - new Date(a.date)
+        );
+      } else if (selectedDate === "Más viejunos") {
+        memesByCategory[category].sort(
+          (a, b) => new Date(a.date) - new Date(b.date)
+        );
+      }
+    }
+
+    // Volvemos a aplanar el objeto para obtener un array de memes filtrados y ordenados
+    return Object.values(memesByCategory).flat();
+  };
+
+  // Aplicamos filtros cada vez que cambiamos
+  useEffect(() => {
+    // Otenem memes combinados
+    const allMemes = Object.values(memesByCategory).flat();
+
+    // Llamamos a la función de filtrado y ordenamiento
+    const filtered = filterAndSortMemes(
+      allMemes,
+      selectedCategory,
+      selectedPopularity,
+      selectedDate
+    );
+
+    setFilteredMemes(filtered);
+  }, [selectedCategory, selectedPopularity, selectedDate, memesByCategory]);
 
   const handleDelete = async (category, id) => {
     try {
@@ -151,88 +151,80 @@ const Home = () => {
     setIsModalOpen(false);
   };
 
-    const refreshMemes = async () => {
-        try {
-            const memesPromises = categories.map((category) =>
-                getMemeByCategory(category)
-            )
-            const memesResults = await Promise.all(memesPromises)
-            const newMemesByCategory = categories.reduce(
-                (acc, category, index) => {
-                    acc[category] = memesResults[index]
-                    return acc
-                },
-                {}
-            )
+  const refreshMemes = async () => {
+    try {
+      const memesPromises = categories.map((category) =>
+        getMemeByCategory(category)
+      );
+      const memesResults = await Promise.all(memesPromises);
+      const newMemesByCategory = categories.reduce((acc, category, index) => {
+        acc[category] = memesResults[index];
+        return acc;
+      }, {});
 
-            // Actualizamos el estado memesByCategory
-            setMemesByCategory(newMemesByCategory)
-        } catch (error) {
-            console.error('Error refreshing memes:', error)
-        }
+      // Actualizamos el estado memesByCategory
+      setMemesByCategory(newMemesByCategory);
+    } catch (error) {
+      console.error("Error refreshing memes:", error);
     }
   };
 
-    return (
-        <div className="min-h-screen w-full m-0">
-            <h1 className="text-4xl font-bold text-center mb-8">
-                Lista de Memes de Gatos
-            </h1>
+  return (
+    <div className="min-h-screen w-full m-0">
+      <h1 className="text-4xl font-bold text-center mb-8">
+        Lista de Memes de Gatos
+      </h1>
 
-            <div className="flex justify-center mb-6">
-                <button
-                    onClick={openModal}
-                    className="relative inline-block text-white p-4 rounded-full bg-gradient-to-br from-purple-800 to-purple-900 shadow-lg hover:shadow-2xl transition-transform duration-300 ease-in-out transform hover:scale-105 animate-spin-slow"
-                >
-                    Crear Nuevo Meme
-                </button>
-            </div>
+      <div className="flex justify-center mb-6">
+        <button
+          onClick={openModal}
+          className="relative inline-block text-white p-4 rounded-full bg-gradient-to-br from-purple-800 to-purple-900 shadow-lg hover:shadow-2xl transition-transform duration-300 ease-in-out transform hover:scale-105 animate-spin-slow"
+        >
+          Crear Nuevo Meme
+        </button>
+      </div>
 
-            {categories.map((category) => {
-                // Filtramos los memes para esta categoría
-                const memesForCategory = filteredMemes.filter(
-                    (meme) => meme.category === category
-                )
+      {categories.map((category) => {
+        // Filtramos los memes para esta categoría
+        const memesForCategory = filteredMemes.filter(
+          (meme) => meme.category === category
+        );
 
-                // Clase para controlar la visibilidad del bloque
-                const categoryBlockClass =
-                    selectedCategory === 'Todas' ||
-                    selectedCategory === category
-                        ? 'block'
-                        : 'hidden'
+        // Clase para controlar la visibilidad del bloque
+        const categoryBlockClass =
+          selectedCategory === "Todas" || selectedCategory === category
+            ? "block"
+            : "hidden";
 
-                return (
-                    <div
-                        key={category}
-                        className={`w-full py-10 ${categoryClasses[category]} ${categoryBlockClass}`} // Aplicamos la clase de visibilidad
-                    >
-                        {/* Renderizamos el título solo si la categoría está seleccionada o si se seleccionó "Todas" */}
-                        {(selectedCategory === 'Todas' ||
-                            selectedCategory === category) && (
-                            <h1 className="text-3xl font-bold mb-8 text-center text-white">
-                                {categoryTitles[category]}
-                            </h1>
-                        )}
-
-                        {/* Pasamos los memes filtrados para esta categoría a MemeGrid */}
-                        <MemeGrid
-                            memes={memesForCategory}
-                            onDelete={(id) => handleDelete(category, id)}
-                        />
-                    </div>
-                )
-            })}
-
-            {isModalOpen && (
-                <Modal onClose={closeModal}>
-                    <CreateMeme
-                        onClose={closeModal}
-                        onMemeCreated={refreshMemes}
-                    />
-                </Modal>
+        return (
+          <div
+            key={category}
+            className={`w-full py-10 ${categoryClasses[category]} ${categoryBlockClass}`} // Aplicamos la clase de visibilidad
+          >
+            {/* Renderizamos el título solo si la categoría está seleccionada o si se seleccionó "Todas" */}
+            {(selectedCategory === "Todas" ||
+              selectedCategory === category) && (
+              <h1 className="text-3xl font-bold mb-8 text-center text-white">
+                {categoryTitles[category]}
+              </h1>
             )}
-        </div>
-    )
-}
 
-export default Home
+            {/* Pasamos los memes filtrados para esta categoría a MemeGrid */}
+            <MemeGrid
+              memes={memesForCategory}
+              onDelete={(id) => handleDelete(category, id)}
+            />
+          </div>
+        );
+      })}
+
+      {isModalOpen && (
+        <Modal onClose={closeModal}>
+          <CreateMeme onClose={closeModal} onMemeCreated={refreshMemes} />
+        </Modal>
+      )}
+    </div>
+  );
+};
+
+export default Home;
